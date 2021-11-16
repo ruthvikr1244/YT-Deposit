915d95cfe83aeebf46900781986e524e33357433
ruthvikr1244 <post.ruthvik@gmail.com>
2021-11-16 22:36:46 +0530

    16-11-2021

--- yt_app_files/lib/main.dart
+++ yt_app_files/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+//import 'src/authentication.dart';
 
 void main() {
   runApp(const MyApp());
